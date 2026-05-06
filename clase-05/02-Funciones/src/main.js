70398aaaf750a8b559f2ca19c9f548693233cf28
Luciano Nuñez <lucianonahuelnunez@gmail.com>
2026-05-06 09:50:14 -0300

    Initial commit

---
+++ clase-05/02-Funciones/src/main.js
@@ -0,0 +1,23 @@
+import './style.css'
+console.log('Luciano')
+console.log('Maxi')
+console.log('Ailen')
+console.log('Rodrigo')
+console.log('Carolina')
+console.log('Aaron')
+
+function mostrarNombre(nombreAImprimir) {
+  console.log(nombreAImprimir)
+}
+
+mostrarNombre('Gregorio')
+mostrarNombre('Judith')
+mostrarNombre('Melina')
+
+const bienvenida = function(nombreADarLaBienvenida){
+  console.log(`Bienvenido ${nombreADarLaBienvenida}`)
+}
+
+bienvenida('Pedro')
+bienvenida('Juan')
+bienvenida('Diego')
